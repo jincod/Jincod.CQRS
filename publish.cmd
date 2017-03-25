5a607fa5149ff1fcf456596e19cf16a539176acc
jincod <vadim.abdrashitov@gmail.com>
2017-03-25 18:13:07 +0500

    updated nuget package

--- publish.cmd
+++ publish.cmd
@@ -1,2 +1,2 @@
 dotnet pack src\Jincod.CQRS\Jincod.CQRS.csproj --configuration Release --output %CD%
-dotnet nuget push Jincod.CQRS.2.0.0.0.nupkg %API_KEY%
+dotnet nuget push Jincod.CQRS.2.0.0.nupkg -k %API_KEY% -s https://www.nuget.org/api/v2/package

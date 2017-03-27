fb2f7c0b14498d94f02a8d1842eaf866ff8af989
jincod <vadim.abdrashitov@gmail.com>
2017-03-27 12:21:21 +0500

    Added example web app

---
+++ Example/Jincod.CQRS.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Jincod.CQRS.Domain;
+using Jincod.CQRS.Queries;
+using Jincod.CQRS.Commands;
+
+namespace Jincod.CQRS.Web.Controllers
+{
+    [Route("api/[controller]")]
+	public class HomeController : Controller
+	{
+		private readonly IQueryProcessor _queryProcessor;
+		private ICommandProcessor _commandProcessor;
+
+		public HomeController(IQueryProcessor queryProcessor, ICommandProcessor commandProcessor)
+		{
+			_queryProcessor = queryProcessor;
+			_commandProcessor = commandProcessor;
+		}
+
+		[HttpGet]
+		public SimpleEntity Get()
+		{
+			return _queryProcessor
+				.Process<SimpleEntity, SimpleQueryContext>(new SimpleQueryContext());
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody]SimpleCommand command)
+		{
+			_commandProcessor.Process(command);
+
+			return Ok();
+		}
+	}
+}
---
+++ Example/Jincod.CQRS.Web/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Jincod.CQRS.Dependencies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Jincod.CQRS.Web
+{
+    public class Startup
+    {
+        public IContainer ApplicationContainer { get; private set; }
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<CommandsModule>();
+            builder.RegisterModule<QueriesModule>();
+            builder.RegisterModule<CqrsModule>();
+
+            builder.Populate(services);
+
+            this.ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(this.ApplicationContainer);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseMvc();
+        }
+    }
+}

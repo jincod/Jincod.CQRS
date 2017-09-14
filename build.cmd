e1e4068a06a3a8f80346407e8bb593b22cda0339
jincod <vadim.abdrashitov@gmail.com>
2017-09-14 16:07:50 +0500

    updated to .net core 2.0

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 if not exist build.ps1 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "Invoke-WebRequest http://cakebuild.net/download/bootstrapper/windows -OutFile build.ps1"
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {.\build.ps1 %*}"
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command ".\build.ps1 %*"
